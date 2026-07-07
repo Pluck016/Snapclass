403b0993c7e48140b240a9001b07902bcbfa666b
Pluck016 <palakkeshari016@gmail.com>
2026-07-07 18:44:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def main():
 
     join_code = st.query_params.get('join-code')
     if join_code:
-        if st.session_state.login_type != 'student':
-            st.session_state.login_type = 'student'
+        if st.session_state.get('login_type') != 'student':
+            st.session_state['login_type'] = 'student'
             st.rerun()
         if st.session_state.get('is_logged_in')and st.session_state.get('user_role') == 'student':
             auto_enroll_dialog(join_code)
