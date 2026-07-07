3a73848c676c2d042d9e49cc781675848e22cecd
Pluck016 <palakkeshari016@gmail.com>
2026-07-07 20:18:49 +0530

    Update Supabase client initialization logic
    Refactor Supabase client initialization to check for secrets.

--- src/database/config.py
+++ src/database/config.py
@@ -1,8 +1,19 @@
+# import streamlit as st
+
+# from supabase import create_client , Client
+
+# supabase: Client = create_client(
+#     st.secrets["SUPABASE_URL"],
+#     st.secrets["SUPABASE_KEY"]
+# )
+
 import streamlit as st
+from supabase import create_client, Client
 
-from supabase import create_client , Client
+_supabase_url = st.secrets.get("SUPABASE_URL")
+_supabase_key = st.secrets.get("SUPABASE_KEY")
 
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+if _supabase_url and _supabase_key:
+    supabase: Client = create_client(_supabase_url, _supabase_key)
+else:
+    supabase = None
